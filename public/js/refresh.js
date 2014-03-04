995fe14a00ceb665155bf7bb2569ca3296d49a34
Kiana Hui <kianah@stanford.edu>
2014-03-03 16:25:06 -0800

    Added version1 javascript

--- public/js/refresh.js
+++ public/js/refresh.js
@@ -145,6 +145,7 @@ $(document).on("click", "#addActivityButton", function(e) {
 
 
 $(document).on("click", ".rallyBtn", function(e) {
+    ga("send", "event", "rally", "click");
     e.preventDefault();
     var dataID = $(this).closest(".activity").attr("data-id");
     var json = {

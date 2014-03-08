80c753d32bb8494f04573e44ae7fea02065333ab
David Jiang <dnjiang@stanford.edu>
2014-03-08 09:57:49 -0800

    Fixed grid

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ app.get('/', function(req, res){
       Activity.find({$and: [{creator: {$in: friendsIDS}}, {date: {$gt : new Date()}}, {_id: {$nin: curUser.rallies}}]} , function(err, acts){ //just after $in term put a comma then, , {_id: {$nin: curUser.rallies}}
         console.log("printing list of activities" + acts);
 
-        res.render('index', {user: req.user,  hasGrid: false, allActivities: acts, message: req.flash('error'), success: req.flash('success')});
+        res.render('index', {user: req.user,  hasGrid: true, allActivities: acts, message: req.flash('error'), success: req.flash('success')});
       });
     });
   } else {
